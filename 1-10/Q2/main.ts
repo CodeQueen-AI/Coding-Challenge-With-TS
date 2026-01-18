64d9721b814c19c5629afc6e6c4d09fb13fcd5aa
CodeQueen-AI <codeq209@gmail.com>
2026-01-18 22:11:06 +0500

    update

--- 1-10/Q2/main.ts
+++ 1-10/Q2/main.ts
@@ -1,6 +1,6 @@
 // Name Cases: Store a person’s name in a variable, and then print that person’s name in 
 // lowercase, uppercase, and titlecase.
-var firstname = "Sumbal Naz";
+let firstname: string = "Sumbal Naz";
 console.log(firstname.toLocaleUpperCase());
 console.log(firstname.toLocaleLowerCase());
-console.log(firstname.charAt(0).toUpperCase() + firstname.slice(1));
+console.log(firstname.charAt(0).toUpperCase()+firstname.slice(1));
