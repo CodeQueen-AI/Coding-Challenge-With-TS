a362bd9c23fc9109ff4c10e4fe76fdbaa5c77244
CodeQueen-AI <codeq209@gmail.com>
2026-01-19 01:34:07 +0500

    update

--- 31-40/Q37/main.ts
+++ 31-40/Q37/main.ts
@@ -1,10 +1,8 @@
 // Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love 
 // TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different
 //  message.
-function make_shirt(size, message) {
-    if (size === void 0) { size = "Large"; }
-    if (message === void 0) { message = "I love TypeScript"; }
-    console.log("The shirt size is ".concat(size, " and the message on it is: ").concat(message));
+function make_shirt(size: string = "Large", message: string = "I love TypeScript"): void {
+    console.log(`The shirt size is ${size} and the message on it is: ${message}`);
 }
 make_shirt();
 make_shirt("Medium");
--- 31-40/Q39/main.ts
+++ 31-40/Q39/main.ts
@@ -3,10 +3,11 @@
 // "Lahore, Pakistan"
 // Call your function with at least three city-country pairs, and print the value that’s returned.
 // Function to format city and country
-function city_country(city, country) {
-    return "".concat(city, ", ").concat(country);
+function city_country(city: string, country: string): string {
+    return `${city}, ${country}`;
 }
+
 // Calling the function with city-country pairs
 console.log(city_country("Lahore", "Pakistan"));
 console.log(city_country("Paris", "France"));
-console.log(city_country("Sydney", "Australia"));
+console.log(city_country("Sydney", "Australia"));
