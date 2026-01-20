e25695fec94135371b30a8b717c6656744d4b4ee
CodeQueen-AI <codeq209@gmail.com>
2026-01-20 16:19:53 +0500

    update

--- 101-110/Q102/Q102.js
+++
@@ -1,4 +0,0 @@
-//Calculate and log the absolute difference between the number -5 and 5.
-/// Calculates the absolute difference between -5 and 5
-var difference = Math.abs(-5 - 5);
-console.log(difference); // Outputs: 10
---
+++ 21-30/Q29/Q103.js
@@ -0,0 +1,6 @@
+//Write a function that returns a random boolean value, true or false.
+// This function returns a random boolean value
+function getRandomBoolean() {
+    return Math.random() > 0.5; 
+}
+console.log(getRandomBoolean()); 
